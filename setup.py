bc56c64c66a5c45c714299e64891cf6ebaab8c38
Tera Earlywine <dev@teraearlywine.com>
2024-07-19 14:39:25 -0700

    *

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+import os
+
+# Utility function to read the requirements.txt file
+
+def read_requirements():
+    requirements_path = 'requirements.txt'
+    if os.path.isfile(requirements_path):
+        with open(requirements_path) as req:
+            return req.read().splitlines()
+    return []
 
 setup(
     name='cowgirl-ai-file-manager',
@@ -9,14 +19,15 @@ setup(
     url='https://github.com/Cowgirl-AI/file-management',
     author='Tera Earlywine',
     author_email='dev@teraearlywine.com',
-    license='MIT',
+    # license='MIT',
     packages=find_packages(),
+    install_requires=read_requirements(),
     python_requires='>=3.10',
-    # entry_points={
-    #     'console_scripts': [
-    #         'your_command=your_module:main_function',
-    #     ],
-    # },
+    entry_points={
+        'console_scripts': [
+            'mgr=src.main:main',
+        ],
+    },
     include_package_data=True,
     zip_safe=False
 )
