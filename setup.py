42f3dd00af541ee80e8426a6060ea7de930ad260
Tera Earlywine <dev@teraearlywine.com>
2024-07-20 15:13:23 -0700

    *

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_requirements():
 
 setup(
     name="cowgirl-ai",
-    version="1.0.17",
+    version="1.0.18",
     description="Interacting with the Open AI API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
