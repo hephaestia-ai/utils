26aec6e4182fc8b78671cbfb4a5b77d4f14abce2
Tera Earlywine <dev@teraearlywine.com>
2024-07-20 15:07:42 -0700

    *

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,1 @@
-**Full Changelog**: https://github.com/Cowgirl-AI/file-management/compare/v1.0.15...v1.0.16
+**Full Changelog**: https://github.com/Cowgirl-AI/file-management/compare/v1.0.16...v1.0.17
--- src/cli/cli.py
+++ src/cli/cli.py
@@ -6,58 +6,48 @@ from cowgirl_ai.load_files.search import Search
 
 logging.basicConfig(level=logging.INFO, datefmt="%Y-%m-%d", format="%(levelname)s - %(asctime)s - %(message)s")
 
-class CLI:
-    """
-    CLI 
-    ---
 
-    Class for cli related arguments
-    """
-    def __init__(self):
-        self.run()
+def search_files(directory, extension):
+    """Search a directory for file extensions and add data to 'loaded_files'."""
+    stack = Search()
+    stack.search(directory, extension)
+    return stack.data
 
-
-    def search_files(self, directory, extension):
-        """Search a directory for file extensions and add data to 'loaded_files'."""
-        stack = Search()
-        stack.search(directory, extension)
-        return stack.data
-
-    # TODO: fix this -- data is not being uploaded correctly
-    # def load_files(self, vector_id, directory, extension):
-    #     """Using the file manager to batch upload loaded files to specific vector."""
-    #     file_manager = FileManager()
-    #     stack = Search()
-    #     stack.search(directory, extension)
-    #     loaded_files = stack.data
+# TODO: fix this -- data is not being uploaded correctly
+# def load_files(vector_id, directory, extension):
+#     """Using the file manager to batch upload loaded files to specific vector."""
+#     file_manager = FileManager()
+#     stack = Search()
+#     stack.search(directory, extension)
+#     loaded_files = stack.data
 
     #     file_manager.batch_upload(vector_id, loaded_files=loaded_files)
 
-    def run(self):
-        """
-        Set up argparse and run commands
-        """
+def run():
+    """
+    Set up argparse and run commands
+    """
 
-        parser = argparse.ArgumentParser(description="CLI tool for file management.")
-        subparsers = parser.add_subparsers(dest="command")
+    parser = argparse.ArgumentParser(description="CLI tool for file management.")
+    subparsers = parser.add_subparsers(dest="command")
 
-        search_parser = subparsers.add_parser("search-files", help="Search a directory for file extensions")
-        search_parser.add_argument("directory", type=str, help="Directory to search")
-        search_parser.add_argument("extension", type=str, help="File extension to search for")
+    search_parser = subparsers.add_parser("search-files", help="Search a directory for file extensions")
+    search_parser.add_argument("directory", type=str, help="Directory to search")
+    search_parser.add_argument("extension", type=str, help="File extension to search for")
 
-        # batch_upload_parser = subparsers.add_parser("batch-upload", help="Batch upload loaded files to vector")
-        # batch_upload_parser.add_argument("vector_id", type=str, help="ID of the vector store you would like to add files to")
-        # batch_upload_parser.add_argument("directory", type=str, help="Directory to search")
-        # batch_upload_parser.add_argument("extension", type=str, help="File extension to search for")
+    # batch_upload_parser = subparsers.add_parser("batch-upload", help="Batch upload loaded files to vector")
+    # batch_upload_parser.add_argument("vector_id", type=str, help="ID of the vector store you would like to add files to")
+    # batch_upload_parser.add_argument("directory", type=str, help="Directory to search")
+    # batch_upload_parser.add_argument("extension", type=str, help="File extension to search for")
 
-        args = parser.parse_args()
+    args = parser.parse_args()
 
-        if args.command == "search-files":
-            files = self.search_files(args.directory, args.extension)
-            logging.info(f"Files to load: {files}")
-        elif args.command == "batch-upload":
-            self.load_files(args.vector_id, args.directory, args.extension)
+    if args.command == "search-files":
+        files = search_files(args.directory, args.extension)
+        logging.info(f"Files to load: {files}")
+    # elif args.command == "batch-upload":
+        # load_files(args.vector_id, args.directory, args.extension)
 
 
 if __name__=="__main__":
-    CLI()
+    run()
